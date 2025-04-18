5bff2c9e2cedf60eaeb82b13c59e8440c1239d50
deepanshu agarwal <deepanshuagarwal9999@gmail.com>
2025-04-18 12:45:15 +0530

    add: create axios instance with request interceptor for form data

---
+++ config/axios.config.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+// import * as SecureStore from "expo-secure-store";
+
+const axiosInstance = axios.create({
+  baseURL: process.env.EXPO_PUBLIC_BASE_URL || "https://demo.esportx.co.in/api",
+  headers: {
+    ["Content-Type"]: "multipart/form-data",
+    ["api_key"]: process.env.EXPO_PUBLIC_API_KEY,
+  },
+});
+
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    try {
+      //   const token = await SecureStore.getItemAsync("token");
+      //   if (token) {
+      //     config.headers.Authorization = "Bearer " + token;
+      //   }
+      const formData = new FormData();
+      Object.entries(config.data).forEach(([key, value]) => {
+        formData.append(key, value as string);
+      });
+      config.data = formData;
+    } catch (error) {
+      console.log("Error getting token from SecureStore", error);
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+export default axiosInstance;
