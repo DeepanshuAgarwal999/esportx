c85a1ad465e0e3af14368a92045c362a04465657
deepanshu agarwal <deepanshuagarwal9999@gmail.com>
2025-04-18 12:46:16 +0530

    feat: refactor AboutService to use axios for API requests and add privacy policy method

--- services/about-service/index.ts
+++ services/about-service/index.ts
@@ -1,33 +1,24 @@
+import axiosInstance from "@/config/axios.config";
+
 const BASE_URL = process.env.EXPO_PUBLIC_BASE_URL || "https://demo.esportx.co.in/api";
 
 export class AboutService {
   static async contactUs(credentials: { user_id: string; name: string; email: string; phone: string; subject: string; message: string }) {
-    const response = await fetch(`${BASE_URL}/contactUs`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    return response.json();
+    const response = await axiosInstance.post(`/contactUs`, credentials);
+    return response.data;
   }
 
   static async sendFeedback(credentials: { user_id: string; name: string; email: string; phone: string; subject: string; message: string }) {
-    const response = await fetch(`${BASE_URL}/sendFeedback`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(credentials),
-    });
-    return response.json();
+    const response = await axiosInstance.post(`/sendFeedback`, credentials);
+    return response.data;
   }
   static async termsAndConditions() {
-    const response = await fetch(`${BASE_URL}/termsAndConditions`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    return response.json();
+    const response = await axiosInstance.get(`/termsAndConditions`);
+    return response.data;
+  }
+
+  static async privacyPolicy() {
+    const response = await axiosInstance.get(`/privacyPolicy`);
+    return response.data;
   }
 }
