da3b08ad2977818c9557c3ddead67b1a00af34cb
deepanshu agarwal <deepanshuagarwal9999@gmail.com>
2025-04-18 12:46:28 +0530

    feat: refactor AuthService to use axios for API requests

--- services/auth-service/index.ts
+++ services/auth-service/index.ts
@@ -1,50 +1,23 @@
-const BASE_URL = process.env.EXPO_PUBLIC_BASE_URL || "https://demo.esportx.co.in/api";
+import axiosInstance from "@/config/axios.config";
 
 export class AuthService {
   static async login(username: string, password: string) {
-    const response = await fetch(`${BASE_URL}/login`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        username,
-        password,
-      }),
-    });
-    return response.json();
+    const response = await axiosInstance.post(`/login`, { username, password });
+    return response.data;
   }
 
   static async signUp(credentials: { username_bgmi: string; userid_bgmi: string; upi_id: string; phone: string }) {
-    const response = await fetch(`${BASE_URL}/signUp`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(credentials),
-    });
-    return response.json();
+    const response = await axiosInstance.post(`/signUp`, credentials);
+    return response.data;
   }
 
   static async getOTP(phone: string) {
-    const response = await fetch(`${BASE_URL}/getOTP`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ phone }),
-    });
-    return response.json();
+    const response = await axiosInstance.post(`/getOTP`, { phone });
+    return response.data;
   }
 
   static async verifyOTP(phone: string, otp: string) {
-    const response = await fetch(`${BASE_URL}/verifyOTP`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ phone, otp }),
-    });
-    return response.json();
+    const response = await axiosInstance.post(`/verifyOTP`, { phone, otp });
+    return response.data;
   }
 }
