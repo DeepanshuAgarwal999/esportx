8e1a5e3f3137f43e5d6466c2f1bc56dc2312f12d
deepanshu agarwal <deepanshuagarwal9999@gmail.com>
2025-04-18 12:45:35 +0530

    feat: enhance account detail screen with user signup functionality and form validation

--- screens/auth/account-detail.screen.tsx
+++ screens/auth/account-detail.screen.tsx
@@ -8,6 +8,8 @@ import { useAlert } from "@/context/alert-provider";
 import * as SecureStore from 'expo-secure-store'
 import GlobalLayout from "@/components/global/global-layout";
 import { useAuth } from "@/context/auth-provider";
+import { AuthService } from "@/services/auth-service";
+import { AxiosError } from "axios";
 
 type Credentials = {
     username: string;
@@ -23,6 +25,7 @@ const AccountDetailScreen = () => {
         upiId: "",
         phone: "",
     });
+    const [isLoading, setIsLoading] = useState(false);
     const [isChecked, setChecked] = useState(false);
     const [errors, setErrors] = useState<Partial<Credentials>>({});
     const router = useRouter();
@@ -48,13 +51,6 @@ const AccountDetailScreen = () => {
             newErrors.userId = "User ID is required.";
         }
 
-        // Validate upiId
-        if (!credentials.upiId.trim()) {
-            newErrors.upiId = "UPI ID is required.";
-        } else if (!/^[\w.-]+@[\w.-]+$/.test(credentials.upiId)) {
-            newErrors.upiId = "Invalid UPI ID format.";
-        }
-
         // Validate phone
         if (!credentials.phone.trim()) {
             newErrors.phone = "Phone number is required.";
@@ -68,87 +64,127 @@ const AccountDetailScreen = () => {
 
     const handleSubmit = async () => {
         if (!isChecked) {
-            alert('Terms & Conditions', 'Please accept Terms & Conditions')
+            alert('Terms & Conditions', 'Please accept Terms & Conditions to continue')
             return;
         }
 
         if (validateForm()) {
-            await SecureStore.setItemAsync('user', JSON.stringify(credentials));
-            setUser(credentials)
-            router.replace('/');
+            const userCredentials = {
+                username_bgmi: credentials.username,
+                userid_bgmi: credentials.userId,
+                upi_id: credentials.upiId,
+                phone: credentials.phone
+            }
+            try {
+                setIsLoading(true)
+                const user = await AuthService.signUp(userCredentials)
+                if (user) {
+                    SecureStore.setItem('user', JSON.stringify(user.user_details));
+                    setUser(credentials)
+                    setCredentials({
+                        username: "",
+                        userId: "",
+                        upiId: "",
+                        phone: "",
+                    });
+                    router.replace({ pathname: '/(auth)/enter-otp', params: { phone: user.user_details.phone } });
+                }
+            } catch (error: any) {
+                if (error instanceof AxiosError) {
+                    if (error?.response?.status === 422) {
+                        alert('Error', "Either user already exist or Invalid credentials")
+                    }
+                    else {
+                        alert('Error', "Invalid credentials")
+                    }
+                }
+                else {
+                    alert('Error', "Something went wrong. Please try again")
+                }
+            }
+            finally {
+                setIsLoading(false)
+            }
         }
     };
-    const keyboardOffsetHeight = 10
     return (
         <GlobalLayout>
-            <KeyboardAvoidingView
-            behavior="height"
-                keyboardVerticalOffset={keyboardOffsetHeight}
-                style={{
-                    flexGrow: 1,
-                    justifyContent: "center",
-                    alignItems: "center",
-                    paddingVertical: 10,
-                    paddingHorizontal: 10,
-                }}
+            <KeyboardAvoidingView 
+                behavior="height"
+                style={{ flex: 1 }}
             >
-                <View className={`w-72 flex flex-col ${Object.keys(errors).length > 0 ? "gap-1" : "gap-5"}`}>
-                    <InputField
-                        placeholder="BGMI Username"
-                        onChangeText={(text) => setCredentials({ ...credentials, username: text })}
-                        inputClassName="!text-center !py-2.5"
-                        value={credentials.username}
-                        disableFullscreenUI={true}
-                    />
-                    {errors.username && <Text style={{ color: "red", fontSize: 12 }}>{errors.username}</Text>}
+                <ScrollView
+                    contentContainerStyle={{
+                        flexGrow: 1,
+                        justifyContent: "center",
+                        alignItems: "center",
+                        paddingVertical: 20,
+                        paddingHorizontal: 10,
+                        paddingBottom: 10
+                    }}
+                    keyboardShouldPersistTaps="handled"
+                    showsVerticalScrollIndicator={false}
+                    keyboardDismissMode="interactive"
+                    bounces={false}
+                >
+                    <View className={`w-72 flex flex-col ${Object.keys(errors).length > 0 ? "gap-1" : "gap-5"}`}>
+                        <InputField
+                            placeholder="BGMI Username"
+                            onChangeText={(text) => setCredentials({ ...credentials, username: text })}
+                            inputClassName="!text-center !py-2.5"
+                            value={credentials.username}
+                            disableFullscreenUI={true}
+                        />
+                        {errors.username && <Text style={{ color: "red", fontSize: 12 }}>{errors.username}</Text>}
 
-                    <InputField
-                        placeholder="BGMI USER ID"
-                        onChangeText={(text) => setCredentials({ ...credentials, userId: text })}
-                        inputClassName="!text-center !py-2.5"
-                        value={credentials.userId}
-                        disableFullscreenUI={true}
-                    />
-                    {errors.userId && <Text style={{ color: "red", fontSize: 12 }}>{errors.userId}</Text>}
+                        <InputField
+                            placeholder="BGMI USER ID"
+                            onChangeText={(text) => setCredentials({ ...credentials, userId: text })}
+                            inputClassName="!text-center !py-2.5"
+                            value={credentials.userId}
+                            disableFullscreenUI={true}
+                        />
+                        {errors.userId && <Text style={{ color: "red", fontSize: 12 }}>{errors.userId}</Text>}
 
-                    <InputField
-                        placeholder="UPI ID"
-                        onChangeText={(text) => setCredentials({ ...credentials, upiId: text })}
-                        className="w-full"
-                        inputClassName="!text-center !py-2.5"
-                        value={credentials.upiId}
-                        disableFullscreenUI={true}
-                    />
-                    {errors.upiId && <Text style={{ color: "red", fontSize: 12 }}>{errors.upiId}</Text>}
+                        <InputField
+                            placeholder="UPI ID"
+                            onChangeText={(text) => setCredentials({ ...credentials, upiId: text })}
+                            className="w-full"
+                            inputClassName="!text-center !py-2.5"
+                            value={credentials.upiId}
+                            disableFullscreenUI={true}
+                        />
+                        {errors.upiId && <Text style={{ color: "red", fontSize: 12 }}>{errors.upiId}</Text>}
 
-                    <InputField
-                        placeholder="Mobile No."
-                        maxLength={10}
-                        keyboardType="numeric"
-                        onChangeText={(text) => setCredentials({ ...credentials, phone: text })}
-                        inputClassName="!text-center !py-2.5"
-                        value={credentials.phone}
-                        disableFullscreenUI={true}
-                    />
-                    {errors.phone && <Text style={{ color: "red", fontSize: 12 }}>{errors.phone}</Text>}
+                        <InputField
+                            placeholder="Mobile No."
+                            maxLength={10}
+                            keyboardType="numeric"
+                            onChangeText={(text) => setCredentials({ ...credentials, phone: text })}
+                            inputClassName="!text-center !py-2.5"
+                            value={credentials.phone}
+                            disableFullscreenUI={true}
+                        />
+                        {errors.phone && <Text style={{ color: "red", fontSize: 12 }}>{errors.phone}</Text>}
 
-                    <View className="flex flex-row items-center gap-2 ">
-                        <View className="bg-white h-5 w-5 flex justify-center items-center">
-                            <Checkbox
-                                value={isChecked}
-                                onValueChange={setChecked}
-                                color={isChecked ? "#333333" : "#FFFFFF"}
-                                className="accent-white !rounded-none"
-                            />
+                        <View className="flex flex-row items-center gap-2">
+                            <View className="bg-white h-5 w-5 flex justify-center items-center">
+                                <Checkbox
+                                    value={isChecked}
+                                    onValueChange={setChecked}
+                                    color={isChecked ? "#333333" : "#FFFFFF"}
+                                    className="accent-white !rounded-none"
+                                />
+                            </View>
+                            <Text className="text-white">I accept Terms & Conditions</Text>
                         </View>
-                        <Text className="text-white">I accept Terms & Conditions</Text>
                     </View>
-                </View>
-                <View className="w-full flex flex-row justify-end">
-                    <Button onPress={handleSubmit} className="w-32 ">
-                        Submit
-                    </Button>
-                </View>
+                    <View className="w-full flex flex-row justify-end mt-4">
+                        <Button isLoading={isLoading} onPress={handleSubmit} className="w-32">
+                            Submit
+                        </Button>
+                    </View>
+                </ScrollView>
             </KeyboardAvoidingView>
         </GlobalLayout>
     );
