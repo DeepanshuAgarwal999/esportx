a18829ab7633420e4a37012adaf077d129ec234f
deepanshu agarwal <deepanshuagarwal9999@gmail.com>
2025-04-26 13:09:31 +0530

    feat: add Contact Us page with feedback component integration

---
+++ app/(routes)/(about)/contactus/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ContactUsSendFeedback from '@/screens/about/contact-sendfeedback'
+
+const ContactUs = () => {
+    return (
+        <ContactUsSendFeedback isContactPage={true} />
+    )
+}
+
+export default ContactUs
